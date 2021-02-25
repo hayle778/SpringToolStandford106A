1abb849830958aba7c70cc922ac89a23a66876ee
Haylemelekot <54283343+hayle778@users.noreply.github.com>
2021-02-25 00:59:09 -0500

    gg

--- src/week2/Fibonacci.java
+++ src/week2/Fibonacci.java
@@ -4,7 +4,7 @@ import acm.program.*;
 
 public class Fibonacci extends ConsoleProgram {
 	
-	private static final int MAX_TERM_VALUE = 10000;
+	private static final int MAX_TERM_VALUE= 10000;
 	
 	public void run() {
 		print("This program list from the fibonacci sequence:");	{
@@ -12,10 +12,12 @@ public class Fibonacci extends ConsoleProgram {
 			
 			int t1 = 0;
 		  int t2 = 1;
-		  
-		    while(t1 < MAX_TERM_VALUE) {
+	//	  int tnew = 1;
+		    
+		println(t1);
+		  while(t1 < MAX_TERM_VALUE) {
 		    	
-		   // println(t1);
+		   println(t1);
 		   
 		    int t3 = t1 + t2;
 		   // int t4 = t2 + t3;
@@ -24,7 +26,7 @@ public class Fibonacci extends ConsoleProgram {
 		    t1 = t2;
 		    t2 = t3;
 		   
-		    println(t1);
+		    //println(t1);
 		    
 		    
 		    }
