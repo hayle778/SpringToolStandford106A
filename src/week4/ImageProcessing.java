bdce759212a33ed77c0187751c8307f8726404e1
Haylemelekot <54283343+hayle778@users.noreply.github.com>
2021-03-15 21:12:32 -0400

    HH

--- src/week4/ImageProcessing.java
+++ src/week4/ImageProcessing.java
@@ -1,7 +1,42 @@
 package week4;
 
+import java.awt.Image;
+
 import acm.program.GraphicsProgram;
 
 public class ImageProcessing extends GraphicsProgram {
 
+
+private static final int[][] Image = null;
+int[][] array  = Image;
+int width = array[0].length;
+
+
+int height = array.length;
+for(int row=0; row<height;row++) {
+	for(int p1 = 0; p1<width/2; p1++) {
+		
+int p2 = width-p1-
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
